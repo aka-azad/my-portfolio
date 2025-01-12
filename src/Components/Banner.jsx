e6669091d34b7b3bff38d00266e7dbeb85414f10
Ashraf Azad <ashrafazad86@gmail.com>
2025-01-12 15:02:36 +0600

    links updated

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -9,7 +9,7 @@ const Banner = () => {
     >
       <div className="flex flex-col justify-center text-white items-center sm:items-start text-center sm:text-left relative">
         <h2 className="text-4xl font-bold mb-2">
-         FrontEnd <br />
+          FrontEnd <br />
           <span className="special-text text-7xl font-charm">
             Web Developer
           </span>
@@ -18,7 +18,7 @@ const Banner = () => {
         <div className="flex gap-5 items-center">
           <div className="hidden sm:flex justify-center sm:justify-start space-x-4">
             <a
-              href="https://www.facebook.com/yourprofile"
+              href="https://web.facebook.com/ashraf.azad.86"
               target="_blank"
               rel="noopener noreferrer"
               className="text-orange-500 hover:text-orange700 transition duration-200"
@@ -26,7 +26,7 @@ const Banner = () => {
               <FaFacebook size={30} />
             </a>
             <a
-              href="https://github.com/yourprofile"
+              href="https://github.com/aka-azad"
               target="_blank"
               rel="noopener noreferrer"
               className="text-orange-500 hover:text-orange700 transition duration-200"
@@ -34,7 +34,7 @@ const Banner = () => {
               <FaGithub size={30} />
             </a>
             <a
-              href="https://www.linkedin.com/in/yourprofile"
+              href="https://www.linkedin.com/in/ashrafuzzaman-azad-web-student"
               target="_blank"
               rel="noopener noreferrer"
               className="text-orange-500 hover:text-orange700 transition duration-200"
@@ -62,7 +62,7 @@ const Banner = () => {
         />
         <div className="flex sm:hidden justify-center absolute bottom-4 space-x-4">
           <a
-            href="https://www.facebook.com/yourprofile"
+            href="https://web.facebook.com/ashraf.azad.86"
             target="_blank"
             rel="noopener noreferrer"
             className="text-orange-500 hover:text-orange700 transition duration-200"
@@ -70,7 +70,7 @@ const Banner = () => {
             <FaFacebook size={30} />
           </a>
           <a
-            href="https://github.com/yourprofile"
+            href="https://github.com/aka-azad"
             target="_blank"
             rel="noopener noreferrer"
             className="text-orange-500 hover:text-orange700 transition duration-200"
@@ -78,7 +78,7 @@ const Banner = () => {
             <FaGithub size={30} />
           </a>
           <a
-            href="https://www.linkedin.com/in/yourprofile"
+            href="https://www.linkedin.com/in/ashrafuzzaman-azad-web-student"
             target="_blank"
             rel="noopener noreferrer"
             className="text-orange-500 hover:text-orange700 transition duration-200"
